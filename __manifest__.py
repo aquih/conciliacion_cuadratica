5c9f268d85b40373be984338c4ec761d6f4d60dd
rocketgithub <rodrigo@aquih.com>
2026-06-24 18:11:30 -0600

    Licencia

--- __manifest__.py
+++ __manifest__.py
@@ -1,21 +1,15 @@
-# -*- coding: utf-8 -*-
 {
     'name': "Conciliación Cuadrática",
-
     'summary': """ Conciliación Cuadrática """,
-
     'description': """
         Conciliación Cuadrática
     """,
-
     'author': "aquíH",
     'website': "http://www.aquih.com",
-
     'category': 'Uncategorized',
-    'version': '0.4',
-
+    'version': '1.0',
     'depends': ['l10n_gt_extra'],
-
+    'license': 'Other OSI approved licence',
     'data': [
         'security/ir.model.access.csv',
         'wizard/reporte_conciliacion_cuadratica_wizard_views.xml',
@@ -23,7 +17,6 @@
         'views/account_payment_view.xml',
         'views/account_bank_statement_line_views.xml',
     ],
-    
     'demo': [
     ],
 }
